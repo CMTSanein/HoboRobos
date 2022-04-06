f6283cdc4709b2da41bf058c7c0464e3817b5381
CMTSanein <constantinemichaelt@gmail.com>
2022-04-07 02:58:49 +0300

    added some dynamic stuff still in progress

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { robots } from "./robots";
 
-const Card = () => {
+const Card = (props) => {
 	return (
 		<div className=" tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-8">
-			<img src="https://robohash.org/set=set2?200x200" alt="hobos" />
+			<img src={`https://robohash.org/${robots.id}?200x200`} alt="hobos" />
 			<div>
-				<h2>Shmulik Levi</h2>
-				<p>shmulik.levi@gmail.com</p>
+				<h2>{props.name}</h2>
+				<p>{props.email}</p>
 			</div>
 		</div>
 	);
