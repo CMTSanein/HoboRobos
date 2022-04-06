71eccfcf8c1e3ec08355a06340a96d36910a17ef
CMTSanein <constantinemichaelt@gmail.com>
2022-04-07 02:15:44 +0300

    Made the Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+	return (
+		<div className=" tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-8">
+			<img src="https://robohash.org/set=set2?200x200" alt="hobos" />
+			<div>
+				<h2>Shmulik Levi</h2>
+				<p>shmulik.levi@gmail.com</p>
+			</div>
+		</div>
+	);
+};
+
+export default Card;
