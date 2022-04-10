f14c88a5208791ff9b30e24aa890b517779adbfa
CMTSanein <constantinemichaelt@gmail.com>
2022-04-10 03:30:45 +0300

    almost done

--- src/App.js
+++ src/App.js
@@ -2,30 +2,33 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
+import "./App.css";
 
 class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			roobots: robots,
+			robots: robots,
 			searchfield: "",
 		};
 		this.onSearchChange = this.onSearchChange.bind(this);
 	}
 
-	onSearchChange(event) {
-		console.log(event.target.value);
-	}
+	onSearchChange = (event) => {
+		this.setState({ searchfield: event.target.value });
+	};
 
 	render() {
+		const filteredRobots = this.state.robots.filter((robots) => {
+			return robots.name
+				.toLowerCase()
+				.includes(this.state.searchfield.toLowerCase());
+		});
 		return (
 			<div className="tc">
-				<h1>HoboFiends!</h1>
-				<SearchBox
-					searchChange={this.onSearchChange}
-					//onChange={this.searchChange}
-				/>
-				<CardList robots={this.state.roobots} />
+				<h1 className="f1">HoboFriends!</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<CardList robots={filteredRobots} />
 			</div>
 		);
 	}
